1415f40a23a838fb577441fa3ee3c2b25d242c88
bhaktraj <ankitbhaktraj@gmail.com>
2025-01-12 20:42:06 +0530

    docker images

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM python:3.12
 WORKDIR /app
+RUN apt-get update \
+    && apt-get upgrade -y \
+    && apt-get install -y gcc default-libmysqlclient-dev pkg-config \
+    && rm -rf /var/lib/apt/lists/*
 COPY requirements.txt ./
+RUN pip3 install mysqlclient
 RUN pip3 install --no-cache-dir -r requirements.txt
 COPY . .
-EXPOSE 8000
-CMD python manage.py runserver
+RUN python manage.py makemigrations
--- vkonsec/settings.py
+++ vkonsec/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'vkonsec.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vkonsec',
+        'HOST': 'mysql',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'root',
+
     }
 }
 
@@ -122,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
